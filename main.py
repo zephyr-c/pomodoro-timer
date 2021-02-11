10e81dfe815c232a4c405cc04a0c4a200d8103f2
Zephyr Worthington <zephyr@Zephyrs-MacBook-Pro.local>
2021-02-11 11:20:41 -0800

    create tkinter canvas, create tomato img and pack to canvas

--- main.py
+++ main.py
@@ -19,6 +19,12 @@ LONG_BREAK_MIN = 20
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
 window.title("Pomodoro Timer")
+window.config(padx=100, pady=50)
+
+canvas = Canvas(width=200, height=224)
+tomato_img = PhotoImage(file="tomato.png")
+canvas.create_image(103, 112, image=tomato_img)
+canvas.pack()
 
 
 window.mainloop()

26c5894c203391d3e2db3a59865f94eeb759b4d3
Zephyr Worthington <zephyr@Zephyrs-MacBook-Pro.local>
2021-02-11 11:47:04 -0800

    complete UI interface with top label, start button, and reset button

--- main.py
+++ main.py
@@ -25,7 +25,21 @@ canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
 tomato_img = PhotoImage(file="tomato.png")
 canvas.create_image(100, 112, image=tomato_img)
 canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
-canvas.pack()
+canvas.grid(row=1, column=1)
+
+title = Label(text="Timer")
+title.config(fg=GREEN, bg=YELLOW, font=(FONT_NAME, 45))
+title.grid(row=0, column=1)
+
+start_button = Button(text="Start", highlightthickness=0)
+start_button.grid(row=2, column=0)
+
+reset_button = Button(text="Reset", highlightthickness=0)
+reset_button.grid(row=2, column=2)
+
+check_mark = Label(text="✔")
+check_mark.config(fg=GREEN, bg=YELLOW)
+check_mark.grid(row=3, column=1)
 
 
 window.mainloop()

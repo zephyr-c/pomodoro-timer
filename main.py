4e3beea066130e288c4696222752bdd7a1060adf
Zephyr Worthington <zephyr@Zephyrs-MacBook-Pro.local>
2021-02-11 13:00:29 -0800

    implement timer functionality to countdown, implement switch between work and break times, change timer label to read either work or break in different colors corresponding to current time block

--- main.py
+++ main.py
@@ -9,12 +9,41 @@ FONT_NAME = "Courier"
 WORK_MIN = 25
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
-
+reps = 5
 # ---------------------------- TIMER RESET ------------------------------- # 
 
-# ---------------------------- TIMER MECHANISM ------------------------------- # 
+# ---------------------------- TIMER MECHANISM ------------------------------- #
+def start_timer():
+    global reps
+    reps += 1
+    work_sec = WORK_MIN
+    short_break_sec = SHORT_BREAK_MIN
+    long_break_sec = LONG_BREAK_MIN
 
-# ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
+    if reps % 2 != 0:
+        print(reps)
+        title.config(fg=GREEN, text="Work")
+        count_down(work_sec)
+    elif reps % 2 == 0 and reps < 8:
+        print(reps)
+        title.config(fg=PINK, text="Break")
+        count_down(short_break_sec)
+    else:
+        print(reps)
+        title.config(fg=RED, text="Break")
+        count_down(long_break_sec)
+
+# ---------------------------- COUNTDOWN MECHANISM ------------------------------- #
+def count_down(count):
+    global reps
+    count_min = count // 60
+    count_sec = count % 60
+
+    canvas.itemconfig(timer_text, text=f"{count_min:02d}:{count_sec:02d}")
+    if count > 0:
+        window.after(1000, count_down, count - 1)
+    else:
+        start_timer()
 
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
@@ -24,22 +53,22 @@ window.config(padx=100, pady=50, bg=YELLOW)
 canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
 tomato_img = PhotoImage(file="tomato.png")
 canvas.create_image(100, 112, image=tomato_img)
-canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
+timer_text = canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
 canvas.grid(row=1, column=1)
 
 title = Label(text="Timer")
-title.config(fg=GREEN, bg=YELLOW, font=(FONT_NAME, 45))
+title.config(fg=GREEN, bg=YELLOW, font=(FONT_NAME, 50))
 title.grid(row=0, column=1)
 
-start_button = Button(text="Start", highlightthickness=0)
+start_button = Button(text="Start", highlightthickness=0, command=start_timer)
 start_button.grid(row=2, column=0)
 
 reset_button = Button(text="Reset", highlightthickness=0)
 reset_button.grid(row=2, column=2)
 
-check_mark = Label(text="✔")
-check_mark.config(fg=GREEN, bg=YELLOW)
-check_mark.grid(row=3, column=1)
+check_marks = Label(text="✔")
+check_marks.config(fg=GREEN, bg=YELLOW)
+check_marks.grid(row=3, column=1)
 
 
 window.mainloop()
